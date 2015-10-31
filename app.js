15d470b2d842087b024b8bc54cca39120303b7c1
XPeng <xiaopplus@gmail.com>
2015-10-31 21:59:07 +0800

    upgrade code for hexo api

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
-require('hexo').init({command: 'server'});
+var Hexo = require('hexo');
+var hexo = new Hexo(process.cwd(), {});
+
+hexo.init().then(function(){
+   hexo.call('server', {p:80}).then(function(){
+       return hexo.exit();
+   }).catch(function(err){
+       return hexo.exit(err);
+   });
+});
+
 
